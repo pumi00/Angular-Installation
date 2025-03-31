abdae40f7d34854e63c52e7083d837c361c1b8ee
Ivan Perez <pumi0751@gmail.com>
2025-03-31 21:08:46 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: `
+  h1{
+  }
+  `
 })
 export class AppComponent {
-  title = 'angular-17-app';
+  city = 'Barcelona';
 }
